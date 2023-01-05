b25cab4e89fbe5c69a38b3eaa2c68362c0282ab0
Joaquin <joacosanchez20@gmail.com>
2023-01-05 20:20:18 -0300

    change url

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,9 +1,9 @@
 export enum Urls {
-    REGISTER = '/api/authentication/signin',
-    LOGIN = '/api/authentication/login',
-    LOGOUT = '/api/authentication/logout',
-    REFRESH = '/api/authentication/refresh_token',
-    PRICE_LIST = '/api/v1/priceList/main',
-    BUDGET = '/api/v1/budget/main',
-    RECIPES = '/api/v1/recipes/main'
+    REGISTER = 'https://recost-server.onrender.com/api/authentication/signin',
+    LOGIN = 'https://recost-server.onrender.com/api/authentication/login',
+    LOGOUT = 'https://recost-server.onrender.com/api/authentication/logout',
+    REFRESH = 'https://recost-server.onrender.com/api/authentication/refresh_token',
+    PRICE_LIST = 'https://recost-server.onrender.com/api/v1/priceList/main',
+    BUDGET = 'https://recost-server.onrender.com/api/v1/budget/main',
+    RECIPES = 'https://recost-server.onrender.com/api/v1/recipes/main'
 } 
